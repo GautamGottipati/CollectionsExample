ea11ce3be1f01ada72b25f43ed56db13bbd7b7f1
GautamGottipati <gautamrace@gmail.com>
2020-01-31 17:22:41 +0530

    Added files

---
+++ CollectionsExamples/CollectionsExamples/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  COLLECTIONS
+            // Using ArrayList
+
+            //Collectionclass1 cs = new Collectionclass1();
+
+            //CollectiondotGeneric cg = new CollectiondotGeneric();
+
+            //CollectiondotConcurrent cc = new CollectiondotConcurrent();
+
+            CollectionsdotSpec csp = new CollectionsdotSpec();
+            Console.ReadKey();
+            
+
+        }
+    }
+}
